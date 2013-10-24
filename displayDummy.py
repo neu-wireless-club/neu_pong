04976227338bc87004a3e40c446b61d369131290
Bryce Carter <brycedcarter@gmail.com>
2013-10-24 17:45:29 -0400

    added functionality for moving the active pixel with the mouse. will update soon with a cleanup

--- displayDummy.py
+++ displayDummy.py
@@ -1,6 +1,5 @@
 from PyQt4 import QtGui,QtCore,Qt
 import sys
-import time
 
 
 class Pixel(QtGui.QTableWidgetItem):
@@ -9,7 +8,6 @@ class Pixel(QtGui.QTableWidgetItem):
         self.setBackgroundColor(QtCore.Qt.black)
 
     def setState(self,state):
-        print 'yo'
         if state:
             self.setBackgroundColor(QtCore.Qt.red)
         else:
@@ -28,6 +26,8 @@ class GameDisplay(QtGui.QWidget):
         self.setGeometry(300, 300, self.cellWidth*self.numCols+50, self.cellHeight*self.numRows+50)
         self.setWindowTitle('Game')
 
+        self.pointPos = [1,1]
+
         self.grid = QtGui.QTableWidget()
         self.grid.setColumnCount(self.numCols)
         self.grid.setRowCount(self.numRows)
@@ -35,6 +35,7 @@ class GameDisplay(QtGui.QWidget):
         self.grid.verticalHeader().setDefaultSectionSize(self.cellHeight)
         self.grid.horizontalHeader().setResizeMode(2)
         self.grid.verticalHeader().setResizeMode(2)
+        self.grid.setEnabled(False)
         for i in range(self.numRows):
             for j in range(self.numCols):
                 tmp = Pixel()
@@ -43,16 +44,47 @@ class GameDisplay(QtGui.QWidget):
         self.lay.addWidget(self.grid)
         self.setLayout(self.lay)
 
+        self.displayPoint(self.pointPos[0],self.pointPos[1],True)
         self.show()
+
 
     def displayPoint(self,pointX,pointY,setTo) :
         pointY-=1
         pointX-=1
-        pxl = self.grid.item(pointX,pointY)
+        pxl = self.grid.item(pointY,pointX)
         pxl.setState(setTo)
 
+    def movePoint(self,dir):
+        print dir
+        if dir == 'up':
+            self.displayPoint(self.pointPos[0],self.pointPos[1],False)
+            self.pointPos[1] = self.pointPos[1]-1 if self.pointPos[1]>1 else 1
+            self.displayPoint(self.pointPos[0],self.pointPos[1],True)
+        if dir == 'down':
+            self.displayPoint(self.pointPos[0],self.pointPos[1],False)
+            self.pointPos[1] = self.pointPos[1]+1 if self.pointPos[1]<self.numRows else self.numRows
+            self.displayPoint(self.pointPos[0],self.pointPos[1],True)
+        if dir == 'left':
+            print 'left'
+            self.displayPoint(self.pointPos[0],self.pointPos[1],False)
+            self.pointPos[0] = self.pointPos[0]-1 if self.pointPos[0]>1 else 1
+            self.displayPoint(self.pointPos[0],self.pointPos[1],True)
+        if dir == 'right':
+            print 'right'
+            self.displayPoint(self.pointPos[0],self.pointPos[1],False)
+            self.pointPos[0] = self.pointPos[0]+1 if self.pointPos[0]<self.numCols else self.numCols
+            self.displayPoint(self.pointPos[0],self.pointPos[1],True)
 
-
+    def keyPressEvent(self, e):
+        print 'hi'
+        if e.key() == QtCore.Qt.Key_Up:
+            self.movePoint('up')
+        if e.key() == QtCore.Qt.Key_Down:
+            self.movePoint('down')
+        if e.key() == QtCore.Qt.Key_Left:
+            self.movePoint('left')
+        if e.key() == QtCore.Qt.Key_Right:
+            self.movePoint('right')
 def main():
     app = QtGui.QApplication(sys.argv)
     gui = GameDisplay()
